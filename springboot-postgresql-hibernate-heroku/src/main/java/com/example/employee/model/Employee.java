2c60cdad46f16dae8c26535f63f06e74dc9a8ad6
vidsdn <vidsdn@gmail.com>
2022-05-04 22:15:41 -0500

    First commit - SpringBoot Rest API ,postgreSQL, deploy on heroku

---
+++ springboot-postgresql-hibernate-heroku/src/main/java/com/example/employee/model/Employee.java
@@ -0,0 +1,49 @@
+package com.example.employee.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+	
+	@Column(name = "email_id", nullable = false)
+	private String emailId;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String emailId) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
+				+ "]";
+	}
+	
+}
